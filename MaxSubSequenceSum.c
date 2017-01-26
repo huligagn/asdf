471cb783236e3f2dfa3e07c86e1b9274b1a67636
huligang <huligang234@163.com>
2017-01-26 21:31:14 +0800

    add the second algorithm to caculate MaxSubSequenceSum

--- MaxSubSequenceSum.c
+++ MaxSubSequenceSum.c
@@ -17,6 +17,45 @@ int MaxSubSeqenceSum_0(const int A[], int N)
 	return max;
 }
 
+static int MaxSubSum(const int A[], int Left, int Right)
+{
+	int MaxLeftSum, MaxRightSum;
+	int MaxLeftBoarderSum, MaxRightBoarderSum;
+	int LeftBoarderSum, RightBoarderSum;
+	int Center, i;
+
+	if(Left == Right)
+		if(A[Left] > 0)
+			return A[Left];
+		else
+			return 0;
+
+	Center = (Left + Right) / 2;
+	MaxLeftSum = MaxSubSum(A, Left, Center);
+	MaxRightSum = MaxSubSum(A, Center, Right);
+
+	MaxLeftBoarderSum = 0; LeftBoarderSum = 0;
+	for(i = Center; i >= Left; i--) {
+		LeftBoarderSum += A[i];
+		if(LeftBoarderSum > MaxLeftBoarderSum)
+			MaxLeftBoarderSum = LeftBoarderSum;
+	}
+
+	MaxRightBoarderSum = 0; RightBoarderSum = 0;
+	for(i = Center; i <= Right; i++) {
+		RightBoarderSum += A[i];
+		if(RightBoarderSum > MaxRightBoarderSum)
+			MaxRightBoarderSum = RightBoarderSum;
+	}
+
+	return Max3(MaxLeftSum, MaxRightSum, MaxLeftBoarderSum + MaxRightBoarderSum);
+}
+
+int MaxSubSeqenceSum_1(const int A[], int N)
+{
+	return MaxSubSum(A, 0, N - 1);
+}
+
 int main(void)
 {
 	int a[] = {-2, 11, -4, 13, -5, -2};
