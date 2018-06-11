03bbeed90e5e45c29cf9881b88830cda67776a58
huligang <huligang234@163.com>
2018-06-11 11:01:25 +0800

    添加注释，加深理解

--- RL/QLearing/tut/RL_brain.py
+++ RL/QLearing/tut/RL_brain.py
@@ -1,34 +1,43 @@
 import numpy as np
 import pandas as pd
 
+## Q表
 class QLearningTable(object):
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9,e_greedy=0.9):
-        self.actions = actions
-        self.lr = learning_rate
-        self.gamma = reward_decay
-        self.epsilon = e_greedy
-        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
 
+    ## 初始化所有参数
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        self.actions = actions      # 选择
+        self.lr = learning_rate     # 学习率
+        self.gamma = reward_decay   # 潜在影响因子，下一步对当前选择的影响因子
+        self.epsilon = e_greedy     # epsilon greedy参数
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64) # Q表
+
+
+    ## 选择下一步的行动
     def choose_action(self, observation):
-        self.check_state_exist(observation)
-        if np.random.uniform() < self.epsilon:
-            state_action = self.q_table.ix[observation, :]
-            state_action = state_action.reindex(np.random.permutation(state_action.index))
-            action = state_action.idxmax()
+        self.check_state_exist(observation)     # 判断当前状态是否存在于Q表，没有则加入
+        if np.random.uniform() < self.epsilon:  # epsilon greedy
+            state_action = self.q_table.ix[observation, :]  # 获取当前状态
+            state_action = state_action.reindex(np.random.permutation(state_action.index))  # 重排序？
+            action = state_action.idxmax()      # 获取下一步动作
         else:
-            action = np.random.choice(self.actions)
+            action = np.random.choice(self.actions) # 随机选择下一步动作
         return action
 
+
+    ## 参数更新的过程
     def learn(self, s, a, r, s_):
-        self.check_state_exist(s_)
-        q_predict = self.q_table.ix[s, a]
+        self.check_state_exist(s_)  # 判断下一个状态是否存在
+        q_predict = self.q_table.ix[s, a]   # 估计收益
         if s_ != 'terminal':
-            q_target = r + self.gamma * self.q_table.ix[s_, :].max()
+            q_target = r + self.gamma * self.q_table.ix[s_, :].max()    # 现实收益
         else:
             q_target = r
         
-        self.q_table.ix[s, a] += self.lr * (q_target - q_predict)
+        self.q_table.ix[s, a] += self.lr * (q_target - q_predict)       # 参数更新，向估计收益靠拢
 
+
+    ## 检测该状态是否存在， 如果没有那么把该状态加入Q表并初始化为0
     def check_state_exist(self, state):
         if state not in self.q_table.index:
             self.q_table = self.q_table.append(
--- RL/QLearing/tut/test.py
+++ RL/QLearing/tut/test.py
@@ -2,20 +2,21 @@ from maze_env import Maze
 from RL_brain import QLearningTable
 
 
+## 整个算法的过程
 def update():
-    for episode in range(100):
+    for _ in range(100):
         observation = env.reset()
 
         while True:
             env.render()
 
-            action = RL.choose_action(str(observation))
+            action = RL.choose_action(str(observation))     # 选择下一步行动
 
-            observation_, reward, done = env.step(action)
+            observation_, reward, done = env.step(action)   # 获取下一步状态、奖励、是否到达
 
-            RL.learn(str(observation), action, reward, str(observation_))
+            RL.learn(str(observation), action, reward, str(observation_))   # 参数更新过程
 
-            observation = observation_
+            observation = observation_                      # 状态更新
 
             if done:
                 break
